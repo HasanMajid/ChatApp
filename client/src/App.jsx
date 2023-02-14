1608db9393c654b3a89329d366623850ec0bc47f
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-13 21:46:11 -0500

    only shows chat after joining room

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,34 +8,39 @@ const socket = io.connect("http://localhost:3000");
 function App() {
   const [username, setUsername] = useState(0);
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true)
     }
   };
 
   return (
     <div className="App">
-      <div className="joinChatContainer">
-        <h3>Join A Chat</h3>
-        <input
-          type="text"
-          placeholder="John..."
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Room ID..."
-          onChange={(e) => {
-            setRoom(e.target.value);
-          }}
-        />
-        <button onClick={joinRoom}>Join A Room</button>
-      </div>
-      <Chat socket={socket} username={username} room={room} />
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
 }
