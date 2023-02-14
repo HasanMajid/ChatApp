ad5cc508e8f92edcb6771d8f1a5cbfa1f1609816
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-13 21:55:09 -0500

    displaying sent text on screen

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -14,23 +15,27 @@ function Chat({ socket, username, room }) {
           ":" +
           new Date(Date.now()).getMinutes(),
       };
-      await socket.emit("send_message", messageData)
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
     }
   };
 
   useEffect(() => {
-    socket.on("receive_message", (data)=>{
-      console.log(data)
-    })
-  }, [socket])
-  
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 
   return (
     <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        {messageList.map((messageContent) => {
+          return <h1>{messageContent.message}</h1>;
+        })}
+      </div>
       <div className="chat-footer">
         <input
           type="text"
