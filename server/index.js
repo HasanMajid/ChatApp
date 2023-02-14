5d799e60c7eaadc370cb766b327d71be9615610d
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-13 23:07:15 -0500

    changed header to website url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://127.0.0.1:5173"],
+    // origin: ["http://127.0.0.1:5173", "https://chat-app-five-self.vercel.app"],
+    origin: "https://chat-app-five-self.vercel.app",
     methods: ["GET", "POST"],
   },
 });
