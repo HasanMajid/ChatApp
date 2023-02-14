9f0f61bc606f258cfec49462dbe1032bd807bd46
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-14 18:13:05 -0500

    header now depends on production env

--- server/index.js
+++ server/index.js
@@ -6,12 +6,15 @@ const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
+var env = process.env.NODE_ENV || "development";
 
 const io = new Server(server, {
   cors: {
-    // origin: ["http://127.0.0.1:5173", "https://chat-app-five-self.vercel.app"],
-    // origin: "https://chat-app-five-self.vercel.app",
-    origin: "*",
+    origin:
+      env === "production"
+        ? "https://chat-app-five-self.vercel.app"
+        : "http://127.0.0.1:5173",
+    // origin: "*",
     methods: ["GET", "POST"],
   },
 });
