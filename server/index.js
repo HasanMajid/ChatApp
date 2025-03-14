705915c5437c6e9fbb756e90c1bd96edc2f2f408
Hasan Majid <hasanmajid3.14@hotmail.com>
2025-03-14 19:54:36 -0400

    refactor: replace Socket.IO with WebSocket for real-time communication

--- server/index.js
+++ server/index.js
@@ -1,39 +1,60 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const { randomUUID } = require('crypto');
 const cors = require("cors");
-const { Server } = require("socket.io");
+const WebSocket = require("ws");
 app.use(cors());
 
-const server = http.createServer(app);
-var env = process.env.NODE_ENV || "development";
+// const server = http.createServer(app);
+// var env = process.env.NODE_ENV || "development";
 
-const io = new Server(server, {
-  cors: {
-    origin:
-      env === "production"
-        ? "https://chat-app-five-self.vercel.app"
-        : "http://127.0.0.1:5173",
-    // origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+const wss = new WebSocket.Server({ port: 8080 });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+const leave = (room, ID) => {
+  console.log("leaving room", room, ID);
+  // not present: do nothing
+  if (!rooms[room]) return;
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  // if the one exiting is the last one, destroy the room
+  if (Object.keys(rooms[room]).length === 1) delete rooms[room];
+  // otherwise simply leave the room
+  else {
+    rooms[room] = rooms[room].filter((ws) => ws.ID !== ID);
+  }
+  console.log("new rooms", rooms);
+};
+
+const rooms = {};
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ID = randomUUID();
+  ws.ID = ID;
+  console.log(ID);
+
+  ws.on("message", (data) => {
+    data = JSON.parse(data);
+    const { message, meta, room } = data;
+    console.log(data);
+
+    if (meta == "join_room"){
+      rooms[room] = rooms[room] ? [...rooms[room], ws] : [ws];
+      console.log(`User with ID: ${ws} joined room: ${room}`);
+      ws.send(JSON.stringify({meta: 'joined_room', room}));
+    }
+  
+    if (meta == "send_message" && message){
+      Object.entries(rooms[room]).forEach(([, sock]) => sock.send(JSON.stringify({ message , meta: 'room_message', room })));
+    }
+
+    if (meta == "leave_room"){
+      console.log(`User with ID: ${ID} left room: ${room}`);
+      Object.keys(rooms).forEach(room => leave(room, ws.ID));
+    }
   });
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
 
-server.listen(3000, () => {
-  console.log("SERVER RUNNING ON PORT 3000");
+
+
+  ws.on("close", () => {
+    console.log("Client disconnected", rooms);
+  });
 });
